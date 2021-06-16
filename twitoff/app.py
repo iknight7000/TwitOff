0c769e1eec9855534682a6fb814b33c1a0b6340b
Ian R Knight <ianknight7000@gmail.com>
2021-06-15 18:29:31 -0700

    Adds more functions to app, and updates database

--- twitoff/app.py
+++ twitoff/app.py
@@ -3,31 +3,64 @@ Main app/routing file for Twitoff.
 The file that holds the function `create_app`
 to collect our modules and organize the flask app.
 """
-
+from os import getenv
 from flask import Flask, render_template
 from .models import DB, User, Tweet
+from .twitter import add_or_update_user
 
 
 def create_app():
 
     # initilizes our application
     app = Flask(__name__)
+    """
+    Flask puts it's configuration values and extention values
+    
+    Config is a subclass of a dictionary and can be modified 
+    like one
+    
+    ENV indicates to Flask, extenstions, and other programs 
+    what context Flask is running in. 
+        - controlled by FLASK_ENV
+        - FLASK_ENV to development will enable debug mode
+            - flask run will use the interactive debugger
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    ENV can behave inconsistently after the app has been set up
+
+    """
+    # use the following database for the connection 
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv["DATABASE_URI"]
+    
+    # Flask SQLalchemy will track modifications of objects and emit signals
+    # default is None
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+    # SQLAlchemy references Flask 
     DB.init_app(app)
 
+    # route decorator tells flask what URL should run our function
     @app.route("/")
+    # Function used on home page
     def root():
         """This will be presented when we visit '<BASE_URL>/ '"""
         insert_example_users()
         users = User.query.all()  # SQL equivalent: `SELECT * FROM user;`
         return render_template("base.html", title='Home', users=users)   
+    
     @app.route("/reset")
+    # Function used on /reset page
+    # Drop all from database and creates it again based on insert _example_users
     def reset():
         DB.drop_all()
         DB.create_all()
+        return render_template("base.html", title="Just Reset")
+
+    @app.route("/update")
+    def update():
+        users = User.query.all()
+        for user in users:
+            add_or_update_user(user.name)
+        return "All the users have been updated!"
 
     return app
 
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,6 +1,8 @@
 """SQLAlchemy models (schema) for twitoff"""
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 
+
+# classes can be mapped to the database in open ended ways
 DB = SQLAlchemy()
 
 
@@ -10,7 +12,9 @@ class User(DB.Model):
     """Twitter Users corresponding to tweets table"""
     # creating id column (primary key)
     id = DB.Column(DB.BigInteger, primary_key=True)
+    # creates name column (string value, no nulls)
     name = DB.Column(DB.String, nullable=False)
+    # newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return f"<User: {self.name}>"
@@ -21,10 +25,31 @@ class User(DB.Model):
 
 class Tweet(DB.Model):
     """Tweet text and data"""
+    # id column (integer values, primary key)
     id = DB.Column(DB.BigInteger, primary_key=True)
+    
+    # text column (unicode values up to 300)
     text = DB.Column(DB.Unicode(300))
+    
+    # vector column (holds python objects, that are serialized)
+    vect = DB.Column(DB.PickleType, nullable=False)
+    
+    
+    # user id column (integer values) ()
+    """
+    1 to many relationship places a foreign key on child 
+    table referencing the parent
+
+    .relationship() is specified on the parent for referencing 
+    a collection of items represented by the child. 
+    """
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'))
+    
+    # Is this supposed to be capitalized?
+    # Is user the parent or child? 
     user = DB.relationship("User", backref=DB.backref('tweets', lazy=True))
 
+    # __repr__ method represents class's objects as a string
     def __repr__(self):
-        return f"<User: {self.text}>"
+        # returns the text of a tweet
+        return f"<Tweet: {self.text}>"
